084e4648c2ae8b0249414e7e2461d35fd9f5fc35
akademik <akademuk24@gmail.com>
2025-10-18 16:09:40 +0300

    add header and burger menu

--- scripts/Header.js
+++ scripts/Header.js
@@ -2,7 +2,9 @@ class Header {
     selectors = {
         root: "[data-js-header]",
         overlay: "[data-js-header-overlay]",
-        burgerButton: "[data-js-header-burger-button]"
+        burgerButton: "[data-js-header-burger-button]",
+        subMenu: "[data-js-header-submenu]",
+        linkSubMenu: "[data-js-header-submenu-link]"
     };
 
     stateClasses = {
@@ -14,7 +16,10 @@ class Header {
         this.rootElement = document.querySelector(this.selectors.root);
         this.overlayElement = document.querySelector(this.selectors.overlay);
         this.burgerButtonElement = document.querySelector(this.selectors.burgerButton);
+        this.subMenuElements = document.querySelectorAll(this.selectors.subMenu);
+        this.linkSubMenuElements = document.querySelectorAll(this.selectors.linkSubMenu);
         this.bindEvents();
+        this.linkEvents();
     }
 
     onBurgerButtonClick = () => {
@@ -23,8 +28,20 @@ class Header {
         document.body.classList.toggle(this.stateClasses.isLock);
     }
 
+    onLinkButtonClick = (event, index) => {
+        if (this.subMenuElements[index]) {
+            this.subMenuElements[index].classList.toggle(this.stateClasses.isActive);
+        }
+    }
+
     bindEvents() {
         this.burgerButtonElement.addEventListener("click", this.onBurgerButtonClick);
+    }
+
+    linkEvents() {
+        this.linkSubMenuElements.forEach((link, index) => {
+            link.addEventListener("click", (event) => this.onLinkButtonClick(event, index));
+        });
     }
 }
 
