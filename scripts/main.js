31760bfec786fb6264c34f303b7f99f21e128753
akademik <akademuk24@gmail.com>
2025-10-19 16:31:53 +0300

    add new slider

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,13 @@
 import Header from "./Header.js";
 import Transformed from "./Transformed.js";
 import Counter from "./Counter.js";
+import Reviews from "./Reviews.js";
 
 new Header();
 
 new Transformed();
 
 new Counter();
+
+new Reviews();
 
